5ead7e12f1581751b7ded32861a10677be37c6f7
Sean Bowker <sbowker@thinkeleven.co.uk>
2024-07-17 11:42:09 +0100

    Add project files.

---
+++ CheckBoxListTest/CheckBoxListItem.cs
@@ -0,0 +1,9 @@
+﻿namespace CheckBoxListTest
+{
+    public class CheckBoxListItem
+    {
+        public int? Value { get; set; }
+        public string Text { get; set; }
+        public bool Selected { get; set; }
+    }
+}
---
+++ CheckBoxListTest/Components/CheckBoxListComponent.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CheckBoxListTest.Components
+{
+    public partial class CheckBoxListComponent
+    {
+		[Parameter]
+        public string? Label { get; set; }
+
+        [Parameter]
+        public int? DefaultSelectedIndex { get; set; }
+
+        private void CheckboxClicked(string selectedValue, object? isChecked)
+        {
+            if (CurrentValue is not null)
+            {
+                var selectedItem = CurrentValue.FirstOrDefault(x => x.Text.Equals(selectedValue));
+
+                if (selectedItem is not null && isChecked is not null)
+                {
+                    selectedItem.Selected = (bool)isChecked;
+                }
+
+                if (CurrentValue.All(x => !x.Selected) && DefaultSelectedIndex is not null && CurrentValue.Count > DefaultSelectedIndex)
+                {
+                    CurrentValue[DefaultSelectedIndex.Value].Selected = true;
+                }
+            }
+        }
+
+        protected override bool TryParseValueFromString(string? value, out List<CheckBoxListItem> result, out string validationErrorMessage)
+        {
+            result = [];
+            validationErrorMessage = "";
+            return true;
+        }
+    }
+}
